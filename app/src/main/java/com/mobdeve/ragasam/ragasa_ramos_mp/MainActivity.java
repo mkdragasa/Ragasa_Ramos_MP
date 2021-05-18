7a9e73e2837d293843334f6d526cecb3084eda54
mkdragasa <michaela_ragasa@dlsu.edu.ph>
2021-05-19 00:53:06 +0800

    test commit

--- app/src/main/java/com/mobdeve/ragasam/ragasa_ramos_mp/MainActivity.java
+++ app/src/main/java/com/mobdeve/ragasam/ragasa_ramos_mp/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //hello this is a test push
     }
 }
