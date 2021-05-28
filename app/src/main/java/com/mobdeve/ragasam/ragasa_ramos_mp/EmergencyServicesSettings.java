eaa8167de24fa8456b2a4d83ccf00a75d05c2eba
mkdragasa <michaela_ragasa@dlsu.edu.ph>
2021-05-29 00:13:13 +0800

    Emergency Services Layout

---
+++ app/src/main/java/com/mobdeve/ragasam/ragasa_ramos_mp/EmergencyServicesSettings.java
@@ -0,0 +1,14 @@
+package com.mobdeve.ragasam.ragasa_ramos_mp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmergencyServicesSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency_services_settings);
+    }
+}
--- app/src/main/java/com/mobdeve/ragasam/ragasa_ramos_mp/ui/emergencyServices/EmergencyServicesFragment.java
+++ app/src/main/java/com/mobdeve/ragasam/ragasa_ramos_mp/ui/emergencyServices/EmergencyServicesFragment.java
@@ -3,6 +3,8 @@ package com.mobdeve.ragasam.ragasa_ramos_mp.ui.emergencyServices;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,27 +14,71 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.mobdeve.ragasam.ragasa_ramos_mp.EmergencyServicesSettings;
 import com.mobdeve.ragasam.ragasa_ramos_mp.R;
+import com.mobdeve.ragasam.ragasa_ramos_mp.SafetyGuidelines;
 
 public class EmergencyServicesFragment extends Fragment {
 
-    private EmergencyServicesViewModel EmergencyServicesModel;
+    Activity context;
+    ImageButton fireBtn, earthquakeBtn, floodBtn;
+    Button settingsBtn;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        EmergencyServicesModel =
-                new ViewModelProvider(this).get(EmergencyServicesViewModel.class);
+        context = getActivity();
+
         View root = inflater.inflate(R.layout.fragment_emergency_services, container, false);
-        final TextView textView = root.findViewById(R.id.text_emergency_services);
-        EmergencyServicesModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+
+        return root;
+    }
+
+    public void onStart() {
+        super.onStart();
+
+        fireBtn =  (ImageButton) context.findViewById(R.id.ib_fire);
+        fireBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"Fire Button", Toast.LENGTH_SHORT).show();
             }
         });
-        return root;
+
+        earthquakeBtn =  (ImageButton) context.findViewById(R.id.ib_earthquake);
+        earthquakeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"Earthquake Button", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        floodBtn =  (ImageButton) context.findViewById(R.id.ib_flood);
+        floodBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"Flood Button", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        settingsBtn =  (Button) context.findViewById(R.id.btn_settings);
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(),"Settings Button", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context, EmergencyServicesSettings.class);
+
+                startActivity(intent);
+            }
+        });
+
+
     }
 
 
